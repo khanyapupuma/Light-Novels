4da2b2bce2113c0768208e7b2849d6b03059a929
Siphokuhle <siphokuhlenyana@gmail.com>
2024-08-23 16:46:25 +0200

    :construction:

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ import productsRouter from './router/productsRouter.js'
 import cors from 'cors'
 
 const app =express()
-
-app.use(cors({origin :'http://localhost:8080',credentials:true}))
 let port=process.env.PORT || 5005
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next()
+})
 app.use(express.json())
+app.use(express.static('public'))
+app.use(cors())
 
 app.use('/users',usersRouter)
 app.use('/products',productsRouter)
 
-app.use(express.static('public'))
 
 app.listen(port,()=>{
     console.log("http://localhost:" +port);
